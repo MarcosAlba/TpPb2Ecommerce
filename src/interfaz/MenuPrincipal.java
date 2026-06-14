5a4679d2b5fc874b6b84043c58dfc0f371c29b55
Juanpi134 <juanpitocino_15@hotmail.com>
2026-06-14 00:53:16 -0300

    preparación de la interfaz parcialmente

---
+++ src/interfaz/MenuPrincipal.java
@@ -0,0 +1,11 @@
+package interfaz;
+
+public enum MenuPrincipal {
+	    REGISTRARSE,
+	    INICIAR_SESION,
+	    SALIR,
+	    VER_CATALOGO,
+	    AGREGAR_AL_CARRITO,
+	    VER_CARRITO,
+	    FINALIZAR_COMPRA
+}
